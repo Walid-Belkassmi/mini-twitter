85736f11980a9609b26b0aad59a0097a531ab602
Walid <belkassmi.walid@gmail.com>
2023-02-01 16:00:11 +0100

    create tweet

--- API/Twits.js
+++ API/Twits.js
@@ -1,6 +1,6 @@
 import { API_KEY } from "react-native-dotenv";
 
-const FetchAllTwits = async () => {
+const FetchAllTwits = async (token) => {
   console.log(API_KEY);
 
   const request = await fetch(
@@ -8,13 +8,33 @@ const FetchAllTwits = async () => {
     {
       headers: {
         apikey: API_KEY,
-        Authorization: `Bearer ${API_KEY}`,
+        Authorization: `Bearer ${token}`,
       },
     }
   );
 
   const response = await request.json();
   return response;
+};
+
+const CreateTwit = async (body, token) => {
+  try {
+    const request = await fetch(
+      "https://qqjgrkrdgfbvsiudchwq.supabase.co/rest/v1/tweet",
+      {
+        method: "Post",
+        headers: {
+          apikey: API_KEY,
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Prefer: "return=minimal",
+        },
+        body: JSON.stringify(body),
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { FetchAllTwits };
